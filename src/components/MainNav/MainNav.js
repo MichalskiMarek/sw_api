5e1c20c7c3c45c5f6f5ce9b31832e3a3d66eeb45
Marek <michalskimarek89@gmail.com>
2018-09-16 17:40:02 +0200

    add fetch and movies styling

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import NavItem from "../NavItem";
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
 import Movies from "../Movies";
 
 export default class MainNav extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Switch>
                     <nav>
                         <ul className={'mainNav'}>
                             <NavItem name={'Movies'}/>
@@ -17,7 +16,6 @@ export default class MainNav extends Component {
                         </ul>
                         <Route path={'/Movies'} component={Movies}/>
                     </nav>
-                </Switch>
 
             </BrowserRouter>
         );
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -2,11 +2,28 @@ import React, {Component} from 'react';
 //import {NavLink, BrowserRouter, Route} from 'react-router-dom';
 
 export default class Movies extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://swapi.co/api/films')
+            .then(response=>response.json())
+            .then(response => this.setState({movies: response.results}));
+    }
+
     render() {
+        console.log(this.state.movies);
+        const movies = this.state.movies;
         return (
-            <div>
-                Filmy
-            </div>
+            <ul className={'movieList'}>
+                {movies.map((el,i)=>{
+                   return <li className={'movieTitle'} key={i}>{el.title}</li>
+                })}
+            </ul>
         );
     }
 }
